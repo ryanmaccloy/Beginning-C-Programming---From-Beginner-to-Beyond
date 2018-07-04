a753ad7600407ea0d65f7d731cc4af2ff84937de
Ryan MacCloy <ryan5190@yahoo.com>
2018-07-04 19:03:22 -0400

    My First Program!
    Enter your favorite number between 1 and 100:

---
+++ FirstProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int favorite_number;
+    std::cout << "What is your favorite number 1-100?";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    return 0;
+}
