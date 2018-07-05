3f63e164a98f008f2c1e4b40b7e367a0210e8061
Ryan MacCloy <ryan5190@yahoo.com>
2018-07-05 15:54:02 -0400

    Calculate the area of a room in square feet
    Section 6 Variables

---
+++ AreaOfARoom.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// This program will calulate the area of a room in square feet.
+int main() {
+    
+    cout << "Enter the width of the room: ";
+    int room_width {0};
+    cin >> room_width;
+    
+    cout << "Enter the length of the room: ";
+    int room_length {0};
+    cin >> room_length;
+    
+    cout << "The area of the room is: " << room_width * room_length << "square feet" << endl;
+    return 0;
+}
